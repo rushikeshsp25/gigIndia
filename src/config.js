ea163128ad9fec357e56d108c325e7cfb1e7e314
rushikeshsp25 <rushikeshsp25@gmail.com>
2020-06-22 22:59:03 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    CLIENT_ID: '655819125875-co2vlr335bi4mljp7nboito8gis671iv.apps.googleusercontent.com',
-    API_KEY: 'AIzaSyDt0l7BEcFRuEZ6ZlDjpFHy4XGbGIxUkls',
+    CLIENT_ID: '<your-client-id>',
+    API_KEY: '<your-api-key>',
     DISCOVERY_DOCS: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
     spreadsheetId: "17U0D50gs-hz0tt7UTG7vLL9SSwfB4SKlENtYPVcDXos",
     // SCOPES : "https://www.googleapis.com/auth/spreadsheets.readonly"
